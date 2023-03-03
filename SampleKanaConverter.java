38e285be9508c89136eccc675d98a45b62ca7aee
SayaliPatiil <92857276+SayaliPatiil@users.noreply.github.com>
2023-03-03 11:08:06 +0530

    Update SampleKanaConverter.java

--- SampleKanaConverter.java
+++ SampleKanaConverter.java
@@ -7,6 +7,10 @@ class SampleKanaConverter
         // Check for arg input
         if(args.length < 1) {
             System.out.println("Must specify a term to convert");
+            System.out.println("Must specify a term to convert");
+            System.out.println("Must specify a term to convert");
+            System.out.println("Must specify a term to convert");
+            System.out.println("Must specify a term to convert");
             System.out.println("EXAMPLE:");
             System.out.println("  java SampleKanaConverter ﾃｽﾄ");
             return;
